11fdea7c77082e2eeee62248ceb2dcd44cfe56b0
taylorsieling <taylorasieling@gmail.com>
2021-03-18 11:05:56 -0500

    commit tree 9b54191753bd407414eefc8b8ad9ce2503350206

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const matches = drivers.filter(match => match.toLowerCase() === name.toLowerCase());
+    return matches
+}
+
+function fuzzyMatch(drivers, string) {
+    const fuzzyMatches = drivers.filter(match => match.startsWith(string));
+    return fuzzyMatches
+}
+
+function matchName(drivers, string) {
+    const nameMatches = drivers.filter(match => match.name === string);
+    return nameMatches
+}
